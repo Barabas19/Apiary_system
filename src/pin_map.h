c4734ef4c0ad66f9bc55ae79909739ba74c1197c
Barabas19 <info@dd.9e.cz>
2023-10-18 07:25:32 +0200

    Add pin map

---
+++ src/pin_map.h
@@ -0,0 +1,23 @@
+// Pin map
+//                              GPIO_NUM_0
+//                              GPIO_NUM_2
+#define GSM_PWRKEY_PIN          GPIO_NUM_4          // HW of TTGO T-call v1.3
+#define GSM_RESET_PIN           GPIO_NUM_5          // HW of TTGO T-call v1.3
+#define BUTTON_PIN              GPIO_NUM_12         // Button to wakeup and enter setup mode
+#define ONBOARD_LED_PIN         GPIO_NUM_13         // HW of TTGO T-call v1.3
+#define DHT_PIN                 GPIO_NUM_14
+//                              GPIO_NUM_15
+//                              GPIO_NUM_16         // not connected
+//                              GPIO_NUM_17         // not connected
+#define DHT_DATA_PIN            GPIO_NUM_18
+#define DHT_POWER_PIN           GPIO_NUM_19
+#define I2C_SDA                 GPIO_NUM_21         // HW of TTGO T-call v1.3   I2C_SDA
+#define I2C_SCL                 GPIO_NUM_22         // HW of TTGO T-call v1.3   I2C_SCL
+#define GSM_POWER_PIN           GPIO_NUM_23         // HW of TTGO T-call v1.3
+//                              GPIO_NUM_25
+#define GSM_TX_PIN              GPIO_NUM_26         // HW of TTGO T-call v1.3
+#define GSM_RX_PIN              GPIO_NUM_27         // HW of TTGO T-call v1.3
+#define GSM_DTR_PIN             GPIO_NUM_32         // HW of TTGO T-call v1.3
+#define GSM_RING_PIN            GPIO_NUM_33         // HW of TTGO T-call v1.3
+//                              GPIO_NUM_34
+#define BATTERY_LEVEL_PIN       GPIO_NUM_35         // HW of TTGO T-call v1.3
