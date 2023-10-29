d1eb34b50d136f79b6c83d8a56426c01665b0f06
Barabas19 <info@dd.9e.cz>
2023-10-29 19:45:55 +0100

    Add tests

--- test/embedded/test_gsm_controller/test_gsm_controller.cpp
+++ test/embedded/test_gsm_controller/test_gsm_controller.cpp
@@ -4,6 +4,8 @@
 #include "pin_map.h"
 #include "gsm_controller.h"
 
+const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
 GsmController gsm(Serial1, 
     GSM_TX_PIN, GSM_RX_PIN, GSM_POWER_PIN, GSM_PWRKEY_PIN, GSM_RESET_PIN, GSM_DTR_PIN);
 
@@ -12,15 +14,44 @@ void test_power_on_off() {
     TEST_ASSERT(gsm.powerOnModem());
 }
 
-void test_init_deinit() {
+void test_init() {
     TEST_ASSERT(gsm.init());
+}
+
+void test_get_date_time_location() {
+    struct tm dt;
+    double longitude = 0, latitude = 0;
+    TEST_ASSERT(gsm.getTimeDateLocation(dt, &longitude, &latitude));
+    char buff[64];
+    strcpy(buff, __DATE__);
+    char *token = strtok(buff, " ");
+    TEST_ASSERT(strcmp(months[dt.tm_mon], token));
+    TEST_ASSERT_EQUAL(dt.tm_mday, atoi(strtok(NULL, " ")));
+    TEST_ASSERT_EQUAL(dt.tm_year, atoi(strtok(NULL, " ")));
+}
+
+void test_get_local_date_time() {
+    struct tm dt;
+    TEST_ASSERT(gsm.getLocalDateTime(dt));
+    char buff[64];
+    strcpy(buff, __DATE__);
+    char *token = strtok(buff, " ");
+    TEST_ASSERT(strcmp(months[dt.tm_mon], token));
+    TEST_ASSERT_EQUAL(dt.tm_mday, atoi(strtok(NULL, " ")));
+    TEST_ASSERT_EQUAL(dt.tm_year, atoi(strtok(NULL, " ")));
+}
+
+void test_deinit() {
     TEST_ASSERT(gsm.deinit());
 }
 
 void test_gsm_controller() {
     gsm.powerOnModem();
     RUN_TEST(test_power_on_off);
-    RUN_TEST(test_init_deinit);
+    RUN_TEST(test_init);
+    RUN_TEST(test_get_date_time_location);
+    RUN_TEST(test_get_local_date_time);
+    RUN_TEST(test_deinit);
 }
 
 void setup() {
