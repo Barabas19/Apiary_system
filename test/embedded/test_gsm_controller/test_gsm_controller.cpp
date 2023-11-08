7a2cb4cc6aca772fe86a54053b1e2914dc05311f
Barabas19 <info@dd.9e.cz>
2023-11-08 21:53:03 +0100

    add tests for GET and POST requests

--- test/embedded/test_gsm_controller/test_gsm_controller.cpp
+++ test/embedded/test_gsm_controller/test_gsm_controller.cpp
@@ -1,6 +1,8 @@
 #include <unity.h>
 #include <Arduino.h>
+#include <ArduinoJson.h>
 
+#include "config.h"
 #include "pin_map.h"
 #include "gsm_controller.h"
 
@@ -41,6 +43,34 @@ void test_get_local_date_time() {
     TEST_ASSERT_EQUAL(dt.tm_year, atoi(strtok(NULL, " ")));
 }
 
+void test_send_http_get_request() {
+    char buff[128];
+    char url[128];
+    const char *key = "Hello";
+    const char *value = "Apiary";
+    sprintf(url, "%s/test/http-test.php?%s=%s", CONF_WEB_SERVER, key, value);
+    TEST_ASSERT(gsm.sendHttpGetReq(url, buff, sizeof(buff)));
+    DynamicJsonDocument doc(128);
+    deserializeJson(doc, buff);
+    TEST_ASSERT(doc["method"] == "GET");
+    TEST_ASSERT(doc[key] == value);
+}
+
+void test_send_http_post_request() {
+    char buff[128];
+    char url[128];
+    char data[128];
+    const char *key = "Hello";
+    const char *value = "Apiary";
+    sprintf(url, "%s/test/http-test.php", CONF_WEB_SERVER);
+    sprintf(data, "%s=%s", key, value);
+    TEST_ASSERT(gsm.sendHttpPostReq(url, data, buff, sizeof(buff)));
+    DynamicJsonDocument doc(128);
+    deserializeJson(doc, buff);
+    TEST_ASSERT(doc["method"] == "POST");
+    TEST_ASSERT(doc[key] == value);
+}
+
 void test_deinit() {
     TEST_ASSERT(gsm.deinit());
 }
@@ -51,6 +81,8 @@ void test_gsm_controller() {
     RUN_TEST(test_init);
     RUN_TEST(test_get_date_time_location);
     RUN_TEST(test_get_local_date_time);
+    RUN_TEST(test_send_http_get_request);
+    RUN_TEST(test_send_http_post_request);
     RUN_TEST(test_deinit);
 }
 
